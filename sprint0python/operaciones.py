6963d6a2aa5ef2e2acffbc07ccedf8d92353edc5
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2024-10-01 14:26:37 +0200

    Ejercicio 1 introducción Python

---
+++ sprint0python/operaciones.py
@@ -0,0 +1,21 @@
+
+
+def suma(n1,n2):
+    print("La suma es: ",n1 + n2)
+
+def resta(n1, n2):
+    print("La resta es: ", n1 - n2)
+
+def multiplication(n1, n2):
+    print("La multiplicación es: ", n1*n2)
+
+def division(n1,n2):
+    if n2 == 0:
+        print("No se puede dividir por 0")
+    else:
+        print("La división es: ", n1/n2)
+
+suma(9,2)
+resta(9,2)
+multiplication(9,2)
+division(9,2)
