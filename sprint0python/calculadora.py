84a8ad44fc9aafe291496c61267b72a70e2466d6
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2024-10-08 14:25:18 +0200

    Modificación ejericio 2, la calculadora, en la que añado los comentarios oportunos

--- sprint0python/calculadora.py
+++ sprint0python/calculadora.py
@@ -1,16 +1,18 @@
 from operaciones import suma, resta, multiplication, division
 
-
+#Empezamos con un bucle para seguir ejecutando la calculadora hasta que por teclado le mandemos parar posteriormente
 while True:
+    #Aquí se introducen los valores sobre los que vamos a realizar la operación
     print("Introduce el primer número:")
     n1 = int(input())
     print("Introduce el segundo número:")
     n2 = int(input())
 
-
+    #Elegimos que operación queremos realizar
     print("Que operación quieres realizar? (1. Suma | 2. Resta | 3. Multiplicar | 4. División")
     option=int(input())
 
+    #Depende de lo que hayamos elegido se realiza una operación u otra, si no es ninguna da mensaje de error
     if option == 1:
         result = suma(n1,n2)
     elif option == 2:
@@ -22,11 +24,12 @@ while True:
     else:
         result =print("Operacion erronea")
 
-    print(f"El restulado es:")
+    #Muestra por pantalla el resultado de lo que acabamos de calcular
+    print(f"El restulado es:", result)
 
+    #Elegimos si realizar otra operación o se finaliza el programa.
     print("Quieres realizar otra operacion?")
     option=input()
-
 
     if option == 'n' or option == 'N':
         break
