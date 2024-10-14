359d53f63b66c265b1426221951896c91cc51446
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2024-10-14 16:46:42 +0200

    Modificación ejercicio 1, añadidos comentarios

--- sprint1Tkinter/Ejercicio1.py
+++ sprint1Tkinter/Ejercicio1.py
@@ -1,11 +1,18 @@
-import tkinter as tk
+import tkinter as tk #Importamos la librería de tkinter para interfaces
 
 def cambiar_boton():
+    """
+    Este metodo hace una modificación en el texto de la etiqueta3.
+    """
     etiqueta_3.config(text="Texto cambiado")
 
-root = tk.Tk()
-root.title("Ventana ejercicio 1")
-root.geometry("300x200")
+root = tk.Tk() #Creamos una ventana
+root.title("Ventana ejercicio 1") #Asignamos un título a la ventana
+root.geometry("300x200") #Establecemos un tamaño a la ventana
+"""
+Aquí vienen una serie de etiquetas de texto, las que indica el ejercicio que hay que realizar
+más un botón al final para ejecutar el metodo creado al inicio.
+"""
 etiqueta_1 = tk.Label(root, text = "Hola, bienvenido")
 etiqueta_1.pack()
 etiqueta_2 =tk.Label(root, text = "Marcos")
@@ -14,5 +21,5 @@ etiqueta_3 = tk.Label(root, text="Mensaje inicial")
 etiqueta_3.pack()
 boton = tk.Button(root, text="Click aqui para cambiar mensaje", command=cambiar_boton)
 boton.pack()
-
+ #Inicia el bucle necesario del programa, mientras mantiene la ventana abierta.
 root.mainloop()
