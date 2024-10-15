a4603a71eadad00ea8ef2b212dc2fb2a18a9108e
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2024-10-15 13:57:00 +0200

    Ejercicio 6, listbox

---
+++ sprint1Tkinter/Ejercicio6.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from tkinter import mainloop
+
+def mostrar_fruta():
+    """
+    Metodo para que se ejecute al pulsar el botón, cambiará el texto en la etiqueta para mostar que fruta hemos elegido
+    """
+    options = listbox.curselection() #Obtiene la lista de elementos que forman parte de la lista
+    elementos = [listbox.get(i) for i in options] #Acepta el valor de lo elegido en la lista
+    label.config(text=f"Elegido: {', '.join(elementos)}")
+
+root = tk.Tk() #Creación de la ventana
+root.title("Ejercicio 6") #Titutlo de la ventana
+root.geometry("300x200") #Tamaño de la ventana
+
+#Etiqueta para mostrar que tenemos eledido
+label = tk.Label(root,text="Seleccionaste: Nada")
+label.pack()
+
+#Elementos que formarán parte de la lista
+frutas =["Manzana", "Plátano", "Naranja"]
+
+#Creación y función de la lista
+listbox = tk.Listbox(root,selectmode=tk.SINGLE)
+for fruta in frutas:
+    listbox.insert(tk.END, fruta)
+listbox.pack()
+
+#Botón para cambiar el estado de la lista y que muestre que hemos elegido
+boton = tk.Button(root, text="Mostrar elegido", command=mostrar_fruta)
+boton.pack()
+
+# Inicia el bucle necesario del programa, mientras mantiene la ventana abierta.
+root.mainloop()
