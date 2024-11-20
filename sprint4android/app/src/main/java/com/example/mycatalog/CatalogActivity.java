7da4899f35920955d1d8f470aa4417a4c958c479
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2024-11-20 10:02:39 +0100

    Ejercicio 2, añadido de botón con inten

--- sprint4android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint4android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,6 +1,10 @@
 package com.example.mycatalog;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CatalogActivity extends AppCompatActivity {
-
+    private Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_catalog);
+        Button myButton = findViewById(R.id.Button1);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(context, DetailActivity.class);
+                context.startActivity(myIntent);
+            }
         });
+
     }
 }
