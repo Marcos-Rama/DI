2eb28aefca37a1074638f129657865c9bb771e8c
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2025-01-15 10:04:40 +0100

    Modificación de los xml para que se vean bien, añadido cambio de activity por botón y añadida validación de datos no vacíos

--- Proyecto/app/src/main/java/com/example/warframes/LoginActivity.java
+++ Proyecto/app/src/main/java/com/example/warframes/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.example.warframes;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Context;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,10 +13,20 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button registerButton = findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(context, RegisterActivity.class);
+                context.startActivity(myIntent);
+            }
+        });
     }
 }
--- Proyecto/app/src/main/java/com/example/warframes/RegisterActivity.java
+++ Proyecto/app/src/main/java/com/example/warframes/RegisterActivity.java
@@ -1,6 +1,10 @@
 package com.example.warframes;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import com.google.firebase.auth.FirebaseAuth;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +13,36 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    private Context context = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        findViewById(R.id.registerButton).setOnClickListener(v -> registerUser());
     }
-}
+    private void registerUser() {
+        String name = ((EditText) findViewById(R.id.nameEditText)).getText().toString();
+        String email = ((EditText) findViewById(R.id.emailRegisterEditText)).getText().toString();
+        String password = ((EditText) findViewById(R.id.passwordRegisterEditText)).getText().toString();
+        String repassword = ((EditText) findViewById(R.id.passwordConfirmRegisterEditText)).getText().toString();
+        String telephone = ((EditText) findViewById(R.id.telephoneEditText)).getText().toString();
+        String address = ((EditText) findViewById(R.id.addressEditText)).getText().toString();
+
+        if (name.isEmpty() || email.isEmpty() || password.isEmpty() || repassword.isEmpty() ||
+                telephone.isEmpty() || address.isEmpty()) {
+            Toast.makeText(context, "Algún valor estaba vacío", Toast.LENGTH_SHORT).show();
+        } else {
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(context, "Usuario registrado correctamente.", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(context, "Error en el registro: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+    }
