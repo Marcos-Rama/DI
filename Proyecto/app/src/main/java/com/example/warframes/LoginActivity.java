e18c34dbbc81980264b91d130cee48da48fad690
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2025-01-17 12:53:23 +0100

    Completada la función de login en base a los datos en Firebase

--- Proyecto/app/src/main/java/com/example/warframes/LoginActivity.java
+++ Proyecto/app/src/main/java/com/example/warframes/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 import android.content.Context;
 import androidx.activity.EdgeToEdge;
@@ -12,16 +13,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
     private Context context = this;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Button registerButton = findViewById(R.id.registerButton);
-        Button loginButton = findViewById(R.id.loginButton);
+        mAuth = FirebaseAuth.getInstance();
 
+        Button registerButton = findViewById(R.id.registerButton);
+        findViewById(R.id.loginButton).setOnClickListener(v -> loginUser());
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,14 +34,26 @@ public class LoginActivity extends AppCompatActivity {
                 context.startActivity(myIntent);
             }
         });
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent = new Intent(context, DashboardActivity.class);
-                context.startActivity(myIntent);
-            }
-        });
-
-
     }
+        private void loginUser() {
+            String email = ((EditText) findViewById(R.id.emailEditText)).getText().toString();
+            String password = ((EditText) findViewById(R.id.passwordEditText)).getText().toString();
+
+            if  (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(context, "Email o contraseña vacíos", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(context, "Inicio de sesión exitoso.", Toast.LENGTH_SHORT).show();
+                            Intent myIntent = new Intent(context, DashboardActivity.class);
+                            context.startActivity(myIntent);
+                        } else {
+                            Toast.makeText(context, "Error en autenticación.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+
+
 }
--- Proyecto/app/src/main/java/com/example/warframes/RegisterActivity.java
+++ Proyecto/app/src/main/java/com/example/warframes/RegisterActivity.java
@@ -90,25 +90,24 @@ public class RegisterActivity extends AppCompatActivity {
 
         }
 
+        public static class User {
+            public String uid;
+            public String name;
+            public String email;
+            public String telephone;
+            public String address;
 
-    public static class User {
-        public String uid;
-        public String name;
-        public String email;
-        public String telephone;
-        public String address;
 
+            // Constructor vacío necesario para Firebase
+            public User() {
+            }
 
-        // Constructor vacío necesario para Firebase
-        public User() {
+            public User(String uid, String name,String email, String telephone, String address) {
+                this.uid = uid;
+                this.name = name;
+                this.email = email;
+                this.telephone = telephone;
+                this.address = address;
+            }
         }
-
-        public User(String uid, String name,String email, String telephone, String address) {
-            this.uid = uid;
-            this.name = name;
-            this.email = email;
-            this.telephone = telephone;
-            this.address = address;
-        }
-    }
     }
