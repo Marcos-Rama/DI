353aaeb18359fc67daf58b519ed35aa778d4340f
Marcos-Rama <mramar23@fpcoruna.afundacion.org>
2025-02-03 12:01:01 +0100

    Semana 3: Corrección colores de los botones de favoritos. Creación de un botón en favorites activity para volver al dashboard

--- Proyecto/app/src/main/java/com/example/warframes/views/FavoritesActivity.java
+++ Proyecto/app/src/main/java/com/example/warframes/views/FavoritesActivity.java
@@ -1,8 +1,11 @@
 package com.example.warframes.views;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
@@ -20,6 +23,7 @@ import java.util.ArrayList;
 public class FavoritesActivity extends AppCompatActivity {
     private FavoriteViewModel viewModel;
     private WarframeAdapter adapter;
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,8 @@ public class FavoritesActivity extends AppCompatActivity {
         } else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }
+
+        Button backButton = findViewById(R.id.backButton);
         //Adapter con un listener clics
         adapter = new WarframeAdapter(new ArrayList<>(), warframe -> {
             //Intent a DetailActivity
@@ -52,5 +58,12 @@ public class FavoritesActivity extends AppCompatActivity {
         // Configura el ViewModel
         viewModel = new ViewModelProvider(this).get(FavoriteViewModel.class);
         viewModel.getFavoriteWarframes().observe(this, warframes -> adapter.setWarframe(warframes));
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(context, DashboardActivity.class);
+                context.startActivity(myIntent);
+            }
+        });
     }
 }
